ceba8e8e277e13471e0cd01fea8360a024894d12
Julien Herpin <jherpin@ekino.com>
2016-06-12 23:41:46 +0200

    initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# mocha-exercises
+
+## Etape 1
+
+1. Cloner le projet
+2. Lancer les tests
+3. Corriger le premier test pour que ce soit valide
---
+++ calculator.spec.js
@@ -0,0 +1,21 @@
+import sinon from 'sinon';
+import chai from 'chai';
+import Calculator from './calculator';
+
+const expect = chai.expect;
+
+describe('some tests', function() {
+
+  describe('initialisation tests', function() {
+    it('should be true', () => {
+      expect(true).to.equal(false);
+    });
+  });
+
+});
+
+describe('calculator class', function() {
+
+  // @todo...
+
+});
